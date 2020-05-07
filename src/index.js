694f4176d08e2463146fb249fa4d0cc94f6aa792
riyas <riyasalim123@gmail.com>
2020-05-07 18:06:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./index.css";
+
+
+class RSModel extends React.Component {
+
+
+   render() {
+     return (
+       <div>
+         test
+       </div>
+     );
+   }
+   
+ }
+ 
+ export default App;
+ 
+
+ RSModel.propTypes = {
+  name: PropTypes.string
+};
+
+export default RSModel;
