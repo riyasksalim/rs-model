d3e80f3221ea907cb2531bb7eaf89226dc6323db
riyas <riyasalim123@gmail.com>
2020-05-10 17:23:08 +0530

    Added readme file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { Fragment } from 'react';
 import PropTypes from "prop-types";
+import './index.css';
 
-import "./index.css";
 
 
 class RSModel extends React.Component {
-
-
-   render() {
-     return (
-       <div>
-         test
-       </div>
-     );
-   }
-   
- }
  
- export default App;
- 
+  openModel(){
+    document.getElementById("modal").classList.add("modalclicked");
+    document.getElementById("modelContent").classList.add("modelClickedContent");
+  }
+  closeModel() {
+    document.getElementById("modal").classList.remove("modalclicked");
+    document.getElementById("modelContent").classList.remove("modelClickedContent");
+  }
+  componentDidUpdate(prevProps) {
+    debugger
+    if (this.props.show) {
+      this.openModel();
+    }
+    else{
+      this.closeModel();
+    }
+  }
+  render() {
+    let t="click to close"
+    let nochildren=(<span title={t} onClick={()=>{
+      this.closeModel()
+    }}>{t}</span>)
+    return (
+      <Fragment>
+        <div class="rsmodal" id="modal">
+          <div className={"modal-content"} style={this.props.contentStyle} id="modelContent">
+            {this.props.children ? this.props.children : nochildren}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 
- RSModel.propTypes = {
-  name: PropTypes.string
+}
+
+RSModel.propTypes = {
+  show: PropTypes.bool,
+  contentStyle:PropTypes.object
 };
-
 export default RSModel;
